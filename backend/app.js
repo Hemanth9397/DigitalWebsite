ce6c2998dc09003a0e4aaa4e7998202b4cd77b65
Hemanth9397 <ghemanthkumar03@gmail.com>
2025-07-12 22:45:31 +0300

    added demo prod url

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ import authenticate from "./middleware/authMiddleware.js";
 
 const app = express();
 
-app.use(cors({origin: 'http://localhost:3000', credentials: true}));
+app.use(cors({origin: process.env.CORS_ORIGIN_FRONTEND_URL, credentials: true}));
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
